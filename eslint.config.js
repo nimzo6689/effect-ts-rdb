abfbdd3ad350e7cc4095bc8bb2a0c284182cd1d6
nimzo6689 <nimzo.i.d.6689@gmail.com>
2026-02-11 22:32:56 +0900

    feat: Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+export default tseslint.config(
+  {
+    ignores: ['dist/'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+  prettierConfig,
+);
